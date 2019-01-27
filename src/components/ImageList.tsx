acdeab799f57376e99785854b02d85c390274abf
paulbriar <pbriar@live.com>
2019-01-27 10:11:54 -0400

    V1 implementation complete

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class ImageList extends React.Component {
+class ImageList extends React.Component<any, any> {
     render() {
-        const images = this.props.images.map( img => {
-            return <img src={img.urls.regular} alt={img.title}/>;
+        const images = this.props.images.map( ({id, urls, description}: any) => {
+            return <img key={id} src={urls.regular} alt={description}/>;
         });
         
         return <div>{ images }</div>;
